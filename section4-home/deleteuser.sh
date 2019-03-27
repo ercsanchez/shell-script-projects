16c729c6451dd25681e4b8d4e5edb26665ae6538
ercsanchez <sanchez.erc@gmail.com>
2019-03-26 18:44:59 -0700

    linux shell scripting - jason cannon

---
+++ section4-home/deleteuser.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# show users in local system
+cut -d: -f1 /etc/passwd
+
+# prompt for the user to delete
+read -p 'Type the user you want to delete: ' USER_TO_DELETE
+
+# delete user
+userdel -r ${USER_TO_DELETE}
+
+# show list of users again
+cut -d: -f1 /etc/passwd
+
+echo "check if the user has been deleted"
+
+exit 0
